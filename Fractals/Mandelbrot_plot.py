53812b0c0592b591449fd877c4e627c0b74eb23f
Searge <ashikcygan@gmail.com>
2019-01-02 20:34:44 +0200

    Fractals | Plot the Mandelbrot 3nd iter

--- Fractals/Mandelbrot_plot.py
+++ Fractals/Mandelbrot_plot.py
@@ -1,2 +1,31 @@
 from PIL import Image, ImageDraw
 from mandelbrot import mandelbrot, MAX_ITER
+
+# image size in px
+WIDTH = 600
+HEIGHT = 400
+
+# Plot window
+RE_START = -2
+RE_END = 1
+IM_START = -1
+IM_END = 1
+
+palette = []
+
+im = Image.new('RGB', (WIDTH, HEIGHT), (0, 0, 0))
+draw = ImageDraw.Draw(im)
+
+for x in range(0, WIDTH):
+    for y in range(0, HEIGHT):
+        # convert pixels coordinate to comlex number
+        c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
+        # compute the number of iterations
+        m = mandelbrot(c)
+        # the color depends on the number of iterations
+        color = 255 - int(m * 255 / MAX_ITER)
+        # plot the point
+        draw.point([x, y], (color, color, color))
+
+im.save('/output/mandelbrot_plot.png', 'PNG')
