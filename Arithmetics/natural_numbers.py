c4fcee9a2758a7ba4d3ffc5be13629e4a0eb205e
Searge <ashikcygan@gmail.com>
2019-01-03 14:47:14 +0200

    Arithmetics | Normal numbers n+1 iter

--- Arithmetics/natural_numbers.py
+++ Arithmetics/natural_numbers.py
@@ -1,5 +1,9 @@
 
 N = int(input())
+
+divider = dict((i, i) for i in range(2, 20 + 1))
+
+
 list_int = [int(x) for x in str(N)]
 last_int = int(str(N)[-1:])
 last2_int = int(str(N)[-2:])
@@ -13,10 +17,10 @@ print("Дві ост. цифри", "\t", last2_int)
 def even(N):
     if last2_int == 00:
         print("Дві ост. цифри — нулі", "\t", 'Так')
-    elif (last_int % 2) == 0:
-        print("Остання цифра ділиться на 2", "\t", 'Так')
-    if (last2_int % 4) == 0:
-        print("Дві ост. цифри ділиться на 4", "\t", 'Так')
+    elif (last_int % divider[2]) == 0:
+        print(f"Остання цифра ділиться на {divider[2]} \t")
+    if (last2_int % divider[4]) == 0:
+        print(f"Дві ост. цифри ділиться на {divider[4]}")
 
 
 def odd(N):
@@ -31,6 +35,17 @@ def div5(N):
         print("Остання цифра — 5", "\t", 'Так')
     elif last2_int == (25 or 50 or 75):
         print("Дві ост. цифри 25; 50; 75", "\t", 'Так')
+
+
+if N % divider[2] == N % 3:
+    print("Число ділиться на 6", "\t", 'Так')
+
+logic = ((int(str(N)[:-1]) * 3 + int(str(N)[-1:])) % 7) \
+    and ((int(str(N)[:-2]) * 2 + int(str(N)[-2:])) % 7) \
+    and ((int(str(N)[:-1]) + 5 * int(str(N)[-1:])) % 7)
+
+if logic == 0:
+    print("Число ділиться на 7", "\t", 'Так')
 
 
 def find_divider(N):
