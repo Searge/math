0aeb84b12920fc4263d5ab79262df987865d9bd3
Searge <ashikcygan@gmail.com>
2019-03-09 23:14:19 +0200

    Word to Phone Number :sparkles:

--- Arithmetics/phone.py
+++ Arithmetics/phone.py
@@ -1,15 +1,20 @@
+# Створюємо список літер a..o, та чисел 2..6 (в 3 екземплярах)
 alpha_o: str = [chr(i) for i in range(ord('a'), ord('p'))]
 nums_2_6: int = sorted(list(range(2, 7)) * 3)
 
+# Об'єднуємо списки в словник
 n_first = dict(zip(alpha_o, nums_2_6))
 
+# Від p..z та 7..9 у 4-х
 alpha_z: str = [chr(i) for i in range(ord('p'), ord('z') + 1)]
 nums_7_9: int = sorted([7, 8, 9] * 4)
 
 n_rest = dict(zip(alpha_z, nums_7_9))
 
+# З'єднуємо два списки в один
 d = dict(n_first, **n_rest)
 
+# Відредагований словник числових відповідностей
 num_dict = {'a': 2, 'b': 2, 'c': 2,
             'd': 3, 'e': 3, 'f': 3,
             'g': 4, 'h': 4, 'i': 4,
@@ -19,4 +24,17 @@ num_dict = {'a': 2, 'b': 2, 'c': 2,
             't': 8, 'u': 8, 'v': 8,
             'w': 9, 'x': 9, 'y': 9, 'z': 9}
 
-print(list(num_dict[i] for i in input()))
+# Створюємо числову змінну з введеного слова:
+name = int(''.join(map(str,
+                       list(num_dict[i] for i in input().lower()))))
+
+# список усіх можливих літер з номера:
+letters_from_number = []
+
+for l in str(name):
+    for letter, num in num_dict.items():
+        if num == int(l):
+            letters_from_number.append(letter)
+
+print(f"""Число зі слова: {name}\n \
+      список можливих літер: \n {''.join(letters_from_number)}""")
