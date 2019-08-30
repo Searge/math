a2b4eeb2e88f3688d91383a6e145d316d5336857
Searge <ashikcygan@gmail.com>
2019-08-30 14:26:55 +0300

    Ciphers

---
+++ Cipher/cypher.py
@@ -0,0 +1,24 @@
+def caesar():
+    """The Caesar cipher used in Gravity Falls substitutes 
+    the original letter for the third letter before it.
+    In the case for letters X, Y, and Z, one would have 
+    to cycle through to the beginning of the alphabet. 
+    """
+    pass
+
+def atbash():
+    """Atbash ciphers are decoded by reversing the letters. 
+    For example, an A turns into a Z. """
+    pass
+
+def a1z25():
+    """
+    The A1Z26 cipher is a simple substitution cipher decoded 
+    by substituting the nᵗʰ letter of the alphabet 
+    for given number n. 
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
