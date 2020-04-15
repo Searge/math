05c06b7e29bbbe9b4c85db0f5b2fc121c47138e9
Searge <ashikcygan@gmail.com>
2020-04-15 19:48:47 +0300

    Decimal to binary converter

---
+++ Arithmetics/decimal_to_binary.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+def binary(n):
+    temp = deque()
+
+    while n > 0:
+        if n % 2 == 1:
+            temp.appendleft('1')
+        else:
+            temp.appendleft('0')
+        n //= 2
+
+    temp.appendleft('0b')
+
+    return ''.join(temp)
+
+if __name__ == '__main__':
+    text = 'Enter the number to convert: '
+    converted = binary(int(input(text)))
+    print(converted)
